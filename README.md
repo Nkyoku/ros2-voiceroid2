c3254a845e8c42416a1129517fbd9f9519bfabff
Nkyoku <nkyoku@gmail.com>
2022-01-23 18:28:14 +0900

    非同期で再生するようにした

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ ROS2 wrapper package of VOICEROID2
 __Windows Only__
 
 ## Installation
-1. Install Visual Studio 2019 or later.
+1. Install VOICEROID2 (x64).
+1. Install Python 3.4 (x64) or later.
 1. Install ROS2 foxy or later.
 1. Clone this repository.
 1. Build this repository.  
@@ -16,11 +17,26 @@ __Windows Only__
 1. Run the publisher node.  
    `ros2 run voiceroid2 talker`
 1. There are several parameters.
-   - language : string  
-   Name of the language library.
-   If `language` is not specified, default value will be used.
-   - voice : string  
-   Name of the voice library.  
-   If `voice` is not specified, first found one will be used.
-   - topic_name : string  
-   Topic name that the talker node subscribes.
+   - `language` : string  
+     Name of the language library.  
+     If `language` is not specified, default value will be used.
+   - `voice` : string  
+     Name of the voice library.  
+     If `voice` is not specified, first found one will be used.
+   - `subscribe_topic_name` : string  
+     Topic name that the talker node subscribes.
+     The message type of the topic is `std_msgs/String`.
+     Default : `text`
+   - `publish_topic_name` : string  
+     Topic name that the talker node publishes speech data.
+     The message type of the topic is `std_msgs/ByteMultiArray`.  
+     If `publish_topic_name` is not specified, the speech data will be played by local computer which the talker node runs on.
+   - `phrase_dictionary` : string  
+     Path of the phrase dictionary.  
+     Default : `<Documents filder>/VOICEROID2/フレーズ辞書/user.pdic`
+   - `word_dictionary` : string  
+     Path of the word dictionary.  
+     Default : `<Documents filder>/VOICEROID2/単語辞書/user.wdic`
+   - `symbol_dictionary` : string  
+     Path of the symbol dictionary.  
+     Default : `<Documents filder>/VOICEROID2/記号ポーズ辞書/user.sdic`
--- voiceroid2/talker.py
+++ voiceroid2/talker.py
@@ -1,14 +1,20 @@
+from multiprocessing import get_logger
 import rclpy
 import pip
+import os
 import winsound
+import threading
+from ctypes import *
 from rclpy.node import Node
 import std_msgs.msg
+from rcl_interfaces.msg import ParameterType
+from rcl_interfaces.msg import SetParametersResult
 
 # Import pyvcroid2
-# Install from git if not installed
 try:
     import pyvcroid2
 except:
+    # Install from git if not installed
     pip.main(["install", "git+https://github.com/Nkyoku/pyvcroid2.git"])
     import pyvcroid2
 
@@ -17,27 +23,85 @@ class TalkerNode(Node):
     def __init__(self):
         super().__init__('voiceroid2')
 
-        # Initialize pyvcroid2
-        self.vc = pyvcroid2.VcRoid2()
-        self.vc.loadLanguage("standard")
-        voice_list = self.vc.listVoices()
-        if 0 < len(voice_list):
-            self.vc.loadVoice(voice_list[0])
-        else:
-            raise Exception("No voice library")
+        try:
+            # Initialize pyvcroid2
+            self.vc = pyvcroid2.VcRoid2()
 
+            # Load language library
+            language_name = self.declare_parameter("language", "standard").value
+            self.vc.loadLanguage(language_name)
+            get_logger().info("Language library '{0}' was loaded".format(language_name))
+
+            # Load voice library
+            voice_list = self.vc.listVoices()
+            if len(voice_list) == 0:
+                raise Exception("No voice library")
+            voice_name = self.declare_parameter("voice", voice_list[0]).value
+            self.vc.loadVoice(voice_name)
+            get_logger().info("Voice library '{0}' was loaded".format(voice_name))
+
+            # Load dictionaries
+            rfid = c_char_p(b"\xD0\x9A\xD3\xFD\x8F\x23\xAF\x46\xAD\xB4\x6C\x85\x48\x03\x69\xC7")
+            pwstr = c_wchar_p()
+            windll.shell32.SHGetKnownFolderPath(rfid, c_uint32(0), c_void_p(), byref(pwstr))
+            documents_path = wstring_at(pwstr)
+            windll.ole32.CoTaskMemFree(pwstr)
+            default_pdic_path = documents_path + "\\VOICEROID2\\フレーズ辞書\\user.pdic"
+            default_wdic_path = documents_path + "\\VOICEROID2\\単語辞書\\user.wdic"
+            default_sdic_path = documents_path + "\\VOICEROID2\\記号ポーズ辞書\\user.sdic"
+            pdic_path = self.declare_parameter("phrase_dictionary", default_pdic_path).value
+            wdic_path = self.declare_parameter("word_dictionary", default_wdic_path).value
+            sdic_path = self.declare_parameter("symbol_dictionary", default_sdic_path).value
+            if (pdic_path != default_pdic_path) or os.path.isfile(pdic_path):
+                self.vc.reloadPhraseDictionary(pdic_path)
+                get_logger().info("Phrase dictionary '{0}' was loaded".format(pdic_path))
+            if (wdic_path != default_wdic_path) or os.path.isfile(wdic_path):
+                self.vc.reloadWordDictionary(wdic_path)
+                get_logger().info("Word dictionary '{0}' was loaded".format(wdic_path))
+            if (sdic_path != default_sdic_path) or os.path.isfile(sdic_path):
+                self.vc.reloadSymbolDictionary(sdic_path)
+                get_logger().info("Symbol dictionary '{0}' was loaded".format(sdic_path))
+        except Exception as e:
+            get_logger().error(e)
+            raise e
+
+        # Load settings
+        subscribe_topic_name = self.declare_parameter("subscribe_topic_name", "text").value
+        publish_topic_name = self.declare_parameter("publish_topic_name", None).value
+
+        # Initialize node
         self.subscription = self.create_subscription(
             std_msgs.msg.String,
-            'text',
+            subscribe_topic_name,
             self.text_callback,
             10)
-
-        # prevent unused variable warning
-        self.subscription
+        if publish_topic_name is not None:
+            self.publisher = self.create_publisher(std_msgs.msg.ByteMultiArray, publish_topic_name, 10)
+            get_logger().info("Speech data will be published as topic '{0}'".format(publish_topic_name))
+        else:
+            self.publisher = None
+            get_logger().info("Speech data will be played by local computer")
+        self.add_on_set_parameters_callback(self.parameter_callback)
 
     def text_callback(self, msg):
         text = msg.data
-        speech, tts_events = self.vc.textToSpeech(text)
+        speech, _ = self.vc.textToSpeech(text)
+        if self.publisher is None:
+            # Play sound in worker thread
+            t = threading.Thread(target=self.play_sound, args=(speech,))
+            t.start()
+        else:
+            # Publish sound data
+            msg = std_msgs.msg.ByteMultiArray()
+            msg.data = [speech]
+            self.publisher.publish(msg)
+
+    def parameter_callback(self, params):
+        for param in params:
+            pass
+        return SetParametersResult(successful=True)
+
+    def play_sound(self, speech):
         winsound.PlaySound(speech, winsound.SND_MEMORY)
 
 def main(args=None):
